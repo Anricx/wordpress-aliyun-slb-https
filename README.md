249ff769d1cc6089caadc6ddef0f82925f181c95
dengt <joe.dengtao@gmail.com>
2018-11-21 19:23:22 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# wordpress-aliyun-slb-https
+# aliyun-slb-https
+
 Prevent a redirect loop when Aliyun SLB in HTTPS mode and backend is HTTP
+
+
+
+## Install
+
+Upload plugin files to the`/wp-content/plugins/aliyun-slb-https/`directory OR install the plugin through WordPress plugins page directly. Then, activate it.
---
+++ aliyun-slb-https.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Alien SLB HTTPS
+Plugin URI:  https://github.com/Anricx/wordpress-aliyun-slb-https
+Description: Prevent a redirect loop when Aliyun SLB in HTTPS mode and backend is HTTP.
+Version: 1.0.0
+Author: dengt (joe.dengtao@gmail.com)
+Author URI: https://github.com/anricx
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: aliyun-slb-ssl
+*/
+// fixed The Redirect Loop
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' == $_SERVER['HTTP_X_FORWARDED_PROTO'] ) $_SERVER['HTTPS'] = 'on';
